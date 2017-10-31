b326f63ca88298fc91e3b78c895cb49fd0c0ff2c
Kyle Ramgolam <kyle.ramgolam@mymona.uwi.edu>
2017-10-31 04:13:59 +0000

    Final excercise

--- request.php
+++ request.php
@@ -3,16 +3,31 @@
 // accept a term (keyword)
 // respond with a value
 
-$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<dictionary>
+    <entry>
+        <definition name="bar" author="John">A place that sells alcoholic beverages.</definition>
+    </entry>
+    <entry>
+        <definition name=" definition" author="Gaza">A statement of the exact meaning of a word, especially in a dictionary.</definition>
+    </entry>
+    <entry>
+        <definition name="ajax" author="Kartel">Technique which involves the use of javascript and xml(or JSON)</definition>
+    </entry>
+    <entry>
+        <definition name="html" author="Gazaman">A standard markup language for creating web pages and web applications.</definition>
+    </entry>
+    <entry>
+        <definition name="css" author="Peanut">A stylesheet language used for describing the presentation of a document written in a markup language.</definition>
+    </entry>
+    <entry>
+        <definition name="javascript" author="Genki">A lightweight, interpreted programming language with firts-class function that add interactivity to your website.</definition>
+    </entry>
+    <entry>
+       <definition name="php" author="Halo">A sever-side scripting language, and a powerful tool for making dynamic and interactive website.</definition>
+    </entry>
+</dictionary>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
